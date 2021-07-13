dbf72a3bb0ab212aa94ac7627638ba33fc6b0e0f
Julian Muñoz C <mcjulian95@gmail.com>
2021-07-13 10:39:48 -0300

    insercion sin fotola

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template , request
 from flaskext.mysql import MySQL
 
 app= Flask(__name__)
@@ -24,6 +24,27 @@ def index():
     
     return render_template('empleados/index.html')
 
+@app.route('/create')
+def create():
+   return render_template("empleados/create.html")
+
+
+@app.route('/store', methods=['POST'])
+def storage():
+   _nombre=request.form['txtNombre'] 
+   _correo=request.form['txtCorreo']
+   _foto=request.files['txtFoto']
+   
+   sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL,%s,%s,%s);"
+   datos=(_nombre, _correo, _foto.filename)
+   
+   conn=mysql.connect()
+   cursor=conn.cursor()
+   cursor.execute(sql, datos)
+   conn.commit()
+   return render_template('empleados/index.html')
+   
+   
 
 if __name__=='__main__':
     app.run(debug=True)
